7b3d7cf824009f667180bf1c923f4b8b80a8cd70
Jongkuk Lim <limjk@jmarple.ai>
2023-12-13 16:00:56 +0900

    Modify generate_doxygen.sh to copy current custom_header.html

--- generate_doxygen.sh
+++ generate_doxygen.sh
@@ -10,6 +10,8 @@ versions=(v0.1.0)
 
 current_branch=$(git branch --show-current)
 
+cp docs/custom_style/custom_header.html docs/custom_style/custom_header.html.in
+
 rm -rf html/
 
 # Generate documentation for other versions
@@ -17,14 +19,20 @@ for version in "${versions[@]}"; do
   echo "Generating documentation for version $version"
 
   git checkout "$version" || exit 1
+  cp docs/custom_style/custom_header.html.in docs/custom_style/custom_header.html
   doxygen -q
+  git restore docs/custom_style/custom_header.html
   mkdir -p html_tmp
   mv html "html_tmp/html_$version"
 done
 
 # Generate documentation for latest version with path `/html`
 git checkout main || exit 1
+cp docs/custom_style/custom_header.html.in docs/custom_style/custom_header.html
 doxygen -q
+git restore docs/custom_style/custom_header.html
+
+rm docs/custom_style/custom_header.html.in
 
 for version in "${versions[@]}"; do
   mv "html_tmp/html_$version" "html/$version"
